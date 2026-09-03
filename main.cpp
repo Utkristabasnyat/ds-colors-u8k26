a7782696c0f83e833964b845b61086930864195c
Utkrista Basnyat <YOUR_GITHUB_EMAIL>
2026-09-03 15:31:12 -0700

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Color
@@ -15,6 +16,15 @@ int main()
     cout << "Red: " << color1.red << endl;
     cout << "Green: " << color1.green << endl;
     cout << "Blue: " << color1.blue << endl;
+
+    vector<Color> colors;
+
+    colors.push_back(color1);
+
+    cout << "\nVector Test" << endl;
+    cout << "Red: " << colors[0].red << endl;
+    cout << "Green: " << colors[0].green << endl;
+    cout << "Blue: " << colors[0].blue << endl;
 
     return 0;
 }
